a4f415039958a7309cefb54bf1d3bed6cd7a70f0
Patrick Donelan <pat@patspam.com>
2010-04-14 00:21:21 -0400

    Initial commit

---
+++ lib/Task/WebGUI.pm
@@ -0,0 +1,12 @@
+package Task::WebGUI;
+# ABSTRACT: Install WebGUI dependencies
+
+=head1 DESCRIPTION
+
+Run the following to install all WebGUI dependencies:
+
+ > cpanm Task::WebGUI
+ 
+=cut
+
+1;
