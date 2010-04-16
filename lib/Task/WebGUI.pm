c47ef6ef26d3d54cb51e39530487161ec7927fbb
Patrick Donelan <pat@patspam.com>
2010-04-16 17:25:07 -0400

    Some improvements

--- lib/Task/WebGUI.pm
+++ lib/Task/WebGUI.pm
@@ -6,6 +6,19 @@ package Task::WebGUI;
 Run the following to install all WebGUI dependencies:
 
  > cpanm Task::WebGUI
+
+If Image::Magick fails to install, you may need to download and install it manually
+ tar xvfz ImageMagick.tar.gz
+ cd ImageMagick-*
+ ./configure
+ make && make check && sudo make install
+ cd PerlMagick
+ perl Makefile.PL
+ make test && make install
+
+=head1 SEE ALSO
+
+http://webgui.org
  
 =cut
 
